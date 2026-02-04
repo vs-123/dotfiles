d3a87a35a1fbc19e8c5bb053a9c7b5d53d123ff2
VS-123 <>
2026-02-04 12:33:56 +0000

    install.sh: separate graphical packages
    - factor out graphical packages into its own list and prompt
    - rename `$PACKAGES` to `$PKGS`

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,32 +4,48 @@ echo "####################"
 echo "#  INSTALL SCRIPT  #"
 echo "####################"
 
-PACKAGES="
+PKGS="
    bc \
    btop \
    chezmoi \
+   firefox \
+   git \
+   neovim \
+   xclip \
+   zsh 
+"
+
+GRPHCL_PKGS="
    dbus \
    feh \
    firefox \
    flameshot \
-   git \
    glxinfo \
    kitty \
    mesa \
    mesa-demos \
-   neovim \
    openbox \
    rofi \
-   xclip \
    xorg \
-   zsh 
 "
 
 read -p "INSTALL PACKAGES? (y/N) " user_input
 case "$user_input" in
    [yY])
       echo "INSTALLING..." &&\
-      sudo xbps-install -Su $PACKAGES
+      sudo xbps-install -Syu $PKGS
+      ;;
+   *)
+      echo "NOT INSTALLING"
+      ;;
+esac
+echo ""
+
+read -p "INSTALL GRAPHICAL PACKAGES? (y/N) " user_input
+case "$user_input" in
+   [yY])
+      echo "INSTALLING..." &&\
+      sudo xbps-install -Syu $GRPHCL_PKGS
       ;;
    *)
       echo "NOT INSTALLING"
