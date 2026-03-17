6b602370de005040529f30f7c7664c2e86288ba8
VS-123 <>
2026-03-17 07:33:35 +0000

    nvim: add fluoride.nvim plugin

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -10,6 +10,7 @@ Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/nvim-cmp'
 Plug 'neovim/nvim-lspconfig'
 Plug 'godlygeek/tabular'
+Plug 'Sang-it/fluoride'
 call plug#end()
 ]])
 
@@ -32,7 +33,7 @@ vim.lsp.config.cmake = {
 
 vim.lsp.enable('cmake')
 
--- FOR NVIM PLUGIN CFG
+-- SETUP FOR NEOVIM'S LUA
 require("lazydev").setup({
    library = {
       "~/.local/share/nvim/plugged",
@@ -58,7 +59,6 @@ vim.lsp.config.lua_ls = {
 
 vim.lsp.enable('lua_ls')
 
--- DISABLE SQUIGGLY LINES AND STUFF
 vim.diagnostic.config({ virtual_text = false, signs = false, underline = false })
 
 -----------
@@ -81,4 +81,30 @@ cmp.setup({
    window = { completion = { max_height = 7 } },
    sources = { { name = "nvim_lsp" } },
    completion = { autocomplete = false },
+})
+
+----------------
+--  FLUORIDE  --
+----------------
+
+local fluoride = require("fluoride")
+fluoride.setup({
+   window = {
+      title = "Fluoride",     -- string or false to disable
+      width = 0.3,            -- proportion of terminal width (0-1)
+      height = 0.85,          -- proportion of terminal height (0-1)
+      row = 2,                -- fixed rows from top edge
+      col = 2,                -- fixed cols from right edge
+      border = "single",      -- border style (see below)
+      winblend = 15,          -- transparency (0-100)
+      footer = true,          -- show/hide help footer
+      center_breakpoint = 80, -- switch to centered layout below this width
+   },
+   keymaps = {
+      close = "q",            -- close the window
+      close_alt = "<C-c>",    -- alternative close (set false to disable)
+      jump = "<CR>",          -- jump to code point
+      peek = "gd",            -- peek at code point (center + flash)
+      hover = "K",            -- LSP hover on code point
+   },
 })
